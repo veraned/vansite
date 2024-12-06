5546734b9693c3219c8d36942de5bd13214d0387
veraned <veran@vanillian.us>
2024-12-06 04:31:35 -0500

    + messing with macroblog layouts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,15 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-  };
   
+    eleventyConfig.addCollection("blog", function (collectionApi) {
+      return collectionApi.getFilteredByTag("blog");
+    });
+  
+    return {
+      dir: {
+        input: "src",
+        output: "dist",
+      },
+    };
+
+  };
